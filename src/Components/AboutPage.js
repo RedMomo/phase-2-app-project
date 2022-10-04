bba3545d345e7a15176cdae3e203693668acbb15
Ilker Ciliv <ilkerciliv@gmail.com>
2022-10-04 15:50:24 -0400

    css fix

--- src/Components/AboutPage.js
+++ src/Components/AboutPage.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 function AboutPage() {
-  return <div>AboutPage</div>;
+  return (
+    <div className="about-page">
+      <h3 className="center">This is our plant page.</h3>
+      <h1 className="center">PLANTS</h1>
+    </div>
+  );
 }
 
 export default AboutPage;
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,17 +2,12 @@ import React from "react";
 
 function Header() {
   function homeClick() {
-    console.log("home click!")
-}
+    console.log("home click!");
+  }
   // create function to make clicking the header reroute to home
   return (
     <header onClick={homeClick}>
-      <h1>
-        Seymour's Plant Shop
-        <span className="logo" role="img">
-          🌱
-        </span>
-      </h1>
+      <p className="header-title center green">Seymours Plant Shop</p>
     </header>
   );
 }
